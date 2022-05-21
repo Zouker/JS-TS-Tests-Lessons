332d5b3c220b0d39548baac7d0b1e68a4b5ca215
Denis Shubin <denis.shubin@gmail.com>
2022-05-21 22:36:47 +0300

    05 lesson

---
+++ src/05/05_02.ts
@@ -0,0 +1,13 @@
+import {governmentBuildingsType, HouseType} from '../02/02_02';
+
+export const getStreetsTitlesOfGovernmentsBuildings = (buildings: Array<governmentBuildingsType>) => {
+    return buildings.map(b => b.address.street.title)
+}
+
+export function getStreetsTitlesOfHouses(houses: Array<HouseType>) {
+    return houses.map(h => h.address.street.title)
+}
+
+export function createMessages(houses: Array<HouseType>) {
+    return houses.map(h => `Hello guys from ${h.address.street.title}`)
+}
